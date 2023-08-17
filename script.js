9c076f7585dd0eace64fdf6bb63dc52f40c65ef2
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-08-17 11:02:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
+//aguarda até que o DOM tenha sido carregado antes de executar o código abaixo
 document.addEventListener("DOMContentLoaded", () => {
-    // Aguarda até que o DOM tenha sido carregado antes de executar o código abaixo.
 
+    //referencia os elementos HTML
     const procurar = document.getElementById("procurar");
     const planeta = document.getElementById("planeta");
     const informacoes = document.getElementById("informacoes");
 
-    // Obtém referências para elementos HTML relevantes.
-
+    //define um ouvinte de evento para o botão de busca
     procurar.addEventListener("click", () => {
-        // Define um ouvinte de evento para o botão de busca.
 
-        const nomePlaneta = planeta.value.trim();
-        // Obtém o valor do campo de entrada e remove espaços em branco desnecessários.
+        //armazena o planeta inserido pelo usuário na variável nomePlaneta
+        const nomePlaneta = planeta.value;
 
+        //verifica se o nome do planeta não está vazio
         if (nomePlaneta !== "") {
-            // Verifica se o nome do planeta não está vazio.
-
+    
+            //faz uma solicitação à API SWAPI para as buscar informações sobre os planetas e as transforma em JSON
             fetch(`https://swapi.dev/api/planets/?search=${nomePlaneta}`)
                 .then(response => response.json())
-                // Faz uma solicitação à API SWAPI para buscar informações sobre planetas e a transforma em JSON.
 
+                //manipula os dados obtidos da API
                 .then(data => {
-                    // Manipula os dados obtidos da API.
 
+                    //verifica se existem planetas correspondentes
                     if (data.count > 0) {
-                        // Verifica se há planetas correspondentes.
 
+                        //pega o primeiro planeta correspondente da lista
                         const planet = data.results[0];
-                        // Pega o primeiro planeta correspondente da lista.
 
+                        //cria uma string formatada contendo as informações do planeta
                         const planetaHtml = `
                             <h2>${planet.name}</h2>
                             <p>Climate: ${planet.climate}</p>
                             <p>Terrain: ${planet.terrain}</p>
                             <p>Population: ${planet.population}</p>
                         `;
-                        // Cria uma string formatada contendo as informações do planeta.
 
+                        //obtém os URLs dos filmes associados ao planeta
                         const filmesUrls = planet.films;
-                        // Obtém os URLs dos filmes associados ao planeta.
 
+                        //verifica se existem filmes associados
                         if (filmesUrls.length > 0) {
-                            // Verifica se há filmes associados.
 
+                            //faz solicitações para os URLs dos filmes e as transforma em JSON
                             Promise.all(filmesUrls.map(url => fetch(url)))
                                 .then(responses => Promise.all(responses.map(response => response.json())))
-                                // Faz múltiplas solicitações para os URLs dos filmes e as transforma em JSON.
 
+                                //manipula os objetos de filmes obtidos
                                 .then(filmes => {
-                                    // Manipula os objetos de filmes obtidos.
 
+                                    //cria uma string contendo os títulos dos filmes
                                     const filmesHtml = filmes.map(filme => `<p>${filme.title}</p>`).join("");
-                                    // Cria uma string contendo os títulos dos filmes.
 
+                                    //atualiza o conteúdo da seção de informações com os detalhes do planeta e a lista de filmes
                                     informacoes.innerHTML = planetaHtml + `<h3>Films:${filmesHtml}</h3>`;
-                                    // Atualiza o conteúdo da seção de informações com os detalhes do planeta e a lista de filmes.
                                 });
+
                         } else {
+
+                            //caso não haja filmes associados, exibe uma mensagem
                             informacoes.innerHTML = planetaHtml + "<h3>No movies found for this planet.</h3>";
-                            // Caso não haja filmes associados, exibe uma mensagem apropriada.
                         }
                     } else {
+
+                        //caso nenhum planeta correspondente seja encontrado, exibe uma mensagem.
                         informacoes.innerHTML = "<h2>Planet not found.</h2>";
-                        // Caso nenhum planeta correspondente seja encontrado, exibe uma mensagem.
                     }
                 });
         }
